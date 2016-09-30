982570d2c61a912edf2d762ed006f328ecaca2dd
31521401 <31521401@087928-P33S103.macklabs.br>
2016-09-30 10:28:38 -0300

    Criação da Classe de Teste da Classe TicketMachine

---
+++ Source Code Inspection/test/br/calebe/ticketmachine/test/TicketMachineTest.java
@@ -0,0 +1,22 @@
+package br.calebe.ticketmachine.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31521401
+ */
+public class TicketMachineTest {
+    
+    @Test
+    public void testarInserir(){
+        
+    }
+}
